e7b7fb5744a294ef0e3bc609f9497c6ba4895027
Daniel Vena <deaconvena+github@gmail.com>
2023-05-15 15:30:16 -0400

    version bump, no changes

--- plugin.php
+++ plugin.php
@@ -1,21 +1,26 @@
 <?php
+
 namespace CUMULUS\Wordpress\Hacks;
+
 /**
  * Plugin Name: Ninja Forms Editor Permissions
  * Plugin URI: https://github.com/cumulus-digital/wp-ninja-forms-editor-permissions/
  * Description: Allow editors to manage Ninja Forms.
- * Version: 0.1
+ * Version: 0.2
  * Author: vena
  * License: UNLICENSED
  * GitHub Plugin URI: cumulus-digital/wp-ninja-forms-editor-permissions
  */
 // Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) exit;
-
-
-function nf_subs_capabilities( $cap ) {
-	return 'edit_posts';
+if (! defined('ABSPATH')) {
+    exit;
 }
-\add_filter( 'ninja_forms_admin_parent_menu_capabilities', __NAMESPACE__ . '\\nf_subs_capabilities' ); // Parent Menu
-\add_filter( 'ninja_forms_admin_all_forms_capabilities', __NAMESPACE__ . '\\nf_subs_capabilities' ); // Forms Submenu
-\add_filter( 'ninja_forms_admin_submissions_capabilities', __NAMESPACE__ . '\\nf_subs_capabilities' ); // Submissions Submenu
+
+
+function nf_subs_capabilities($cap)
+{
+    return 'edit_posts';
+}
+\add_filter('ninja_forms_admin_parent_menu_capabilities', __NAMESPACE__ . '\\nf_subs_capabilities'); // Parent Menu
+\add_filter('ninja_forms_admin_all_forms_capabilities', __NAMESPACE__ . '\\nf_subs_capabilities'); // Forms Submenu
+\add_filter('ninja_forms_admin_submissions_capabilities', __NAMESPACE__ . '\\nf_subs_capabilities'); // Submissions Submenu
